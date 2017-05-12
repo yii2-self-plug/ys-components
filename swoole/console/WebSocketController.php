4938762ef6346504fb678a3cc10f7da1ddc1be05
员帅 <he19970108he@163.com>
2017-05-12 18:09:01 +0800

    Update WebSocketController.php

--- swoole/console/WebSocketController.php
+++ swoole/console/WebSocketController.php
@@ -6,7 +6,7 @@
  * Time: 16:29
  */
 
-namespace yuanshuai\yscomponents\console;
+namespace yuanshuai\swoole\console;
 
 
 use yii\console\Controller;
@@ -17,12 +17,61 @@ class WebSocketController extends Controller
     protected $server;
     public function init(){
         $this->setServer();
+        $this->server->setFunction([$this,"setFunction"]);
     }
 
+    /**设置回调函数
+     * @param $server
+     * @return mixed
+     */
+    protected function setFunction($server)
+    {
+        $server->setFunction("open",[$this,"open"])
+            ->setFunction("message",[$this,"message"])
+            ->setFunction("close",[$this,"close"])
+            ->setFunction("request",[$this,"request"]);
+        return $server;
+    }
+
+    /**
+     * 当链接上的时候的回调函数
+     * @param \swoole_websocket_server $server
+     * @param $request
+     */
+    protected function open(\swoole_websocket_server $server, $request){}
+
+    /**
+     * 当客户端发送消息时候的回调函数
+     * @param \swoole_websocket_server $server
+     * @param $frame
+     */
+    protected function message(\swoole_websocket_server $server, $frame){}
+
+    /**
+     * 当客户端断开链接时候的回调函数
+     * @param $ser
+     * @param $fd
+     */
+    protected function close($ser, $fd){}
+
+    /**
+     * 当客户端发送HTTP请求时候的回调函数
+     * @param $request
+     * @param $response
+     */
+    protected function request($request, $response){}
+
+    /**
+     * 设置配置文件中的配置项，默认为websocket
+     */
     protected function setServer(){
         $this->server = \Yii::$app->websocket;
     }
 
+    /**
+     * 服务端启动脚本
+     * @param string $status
+     */
     public function actionRun($status = 'start'){
         switch ($status){
             case ConstHelper::RUN_START :
@@ -44,14 +93,23 @@ class WebSocketController extends Controller
         exit();
     }
 
+    /**
+     * 启动服务
+     */
     protected function start(){
         $this->server->setFunction()->start();
     }
 
+    /**
+     * 停止服务
+     */
     protected function stop(){
         $this->server->stop();
     }
 
+    /**
+     * 重启服务
+     */
     protected function restart(){
         $this->server->restart();
     }
@@ -61,6 +119,5 @@ class WebSocketController extends Controller
         echo "php yii websocket/run ".ConstHelper::RUN_STOP." 停止服务。".PHP_EOL;
         echo "php yii websocket/run ".ConstHelper::RUN_RESTART." 重启服务。".PHP_EOL;
         echo "php yii websocket/run ".ConstHelper::RUN_HELP." 查看帮助。".PHP_EOL;
-//        echo "php yii swoole/run ".ConstHelper::RUN_STATUS." 查看状态。".PHP_EOL;
     }
 }
